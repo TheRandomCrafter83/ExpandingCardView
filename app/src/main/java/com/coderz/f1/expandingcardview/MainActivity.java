95b9605f13bd7f7711f6fa3285568ec19ce0fa6d
TheRandomCrafter83 <williams.carl.83@gmail.com>
2021-11-04 22:58:48 -0400

    Optimized the code a bit

--- app/src/main/java/com/coderz/f1/expandingcardview/MainActivity.java
+++ app/src/main/java/com/coderz/f1/expandingcardview/MainActivity.java
@@ -1,44 +1,30 @@
 package com.coderz.f1.expandingcardview;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
-import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.Transformation;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private final int expandedHeight = 116;
 
     RecyclerView recyclerView = null;
     Button button = null;
 
     private boolean expanded = false;
-    private ArrayList<Bitmap> bitmaps = new ArrayList<Bitmap>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,45 +33,35 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler);
         initRecycler();
         button = findViewById(R.id.button_expand);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                expanded = !expanded;
-                if (expanded) {
-//                    ViewGroup.LayoutParams lp = recyclerView.getLayoutParams();
-//                    lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-//                    recyclerView.setLayoutParams(lp);
-                    expand(recyclerView);
-                    button.setText("Show Less");
-                } else {
-//                    ViewGroup.LayoutParams lp = recyclerView.getLayoutParams();
-//                    lp.height = pixelsToDp(expandedHeight);
-//                    recyclerView.setLayoutParams(lp);
-                    collapse(recyclerView);
-                    button.setText("Show More");
-                }
-                recyclerView.invalidate();
+        button.setOnClickListener(v -> {
+            expanded = !expanded;
+            if (expanded) {
+                expand(recyclerView);
+                button.setText(R.string.show_less);
+            } else {
+                collapse(recyclerView);
+                button.setText(R.string.show_more);
             }
+            recyclerView.invalidate();
         });
     }
 
-    private int pixelsToDp(int pixels) {
-        int dp = (int) (pixels * getResources().getDisplayMetrics().density);
-        return dp;
+    private int pixelsToDp() {
+        return (int) (116 * getResources().getDisplayMetrics().density);
     }
 
     private void initRecycler() {
-        bitmaps = getDumbyList(getApplicationContext());
-        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(this, bitmaps);
+        ArrayList<Bitmap> bitmaps = getDummyList(getApplicationContext());
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(bitmaps);
         recyclerView.setAdapter(recyclerAdapter);
         recyclerAdapter.notifyDataSetChanged();
     }
 
-    //Just some functions for testing and creating dumby data
-    private ArrayList<Bitmap> getDumbyList(Context context) {
-        ArrayList<Bitmap> ret = new ArrayList<Bitmap>();
+    //Just some functions for testing and creating dummy data
+    private ArrayList<Bitmap> getDummyList(Context context) {
+        ArrayList<Bitmap> ret = new ArrayList<>();
         for (int i = 1; i < 16; i++) {
-            Bitmap bmp = drawableToBitmap(getDrawable(context, "image" + Integer.toString(i)));
+            Bitmap bmp = drawableToBitmap(getDrawable(context, "image" + i));
             ret.add(bmp);
         }
         return ret;
@@ -110,22 +86,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void collapse(final View view) {
         view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        final int targetHeight = view.getMeasuredHeight();
         //ValueAnimator valueAnimator = new ValueAnimator.ofInt(view.getMeasuredHeight(),pixelsToDp(expandedHeight));
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(view.getMeasuredHeight(), pixelsToDp(expandedHeight))
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(view.getMeasuredHeight(), pixelsToDp())
                 .setDuration(500);
 
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int animatedValue = (int) animation.getAnimatedValue();
-                ViewGroup.LayoutParams lp = view.getLayoutParams();
-                lp.height = animatedValue;
-                view.setLayoutParams(lp);
-                if (animatedValue == 0) {
-
-                }
-            }
+        valueAnimator.addUpdateListener(animation -> {
+            int animatedValue = (int) animation.getAnimatedValue();
+            ViewGroup.LayoutParams lp = view.getLayoutParams();
+            lp.height = animatedValue;
+            view.setLayoutParams(lp);
         });
 
         valueAnimator.start();
@@ -133,39 +102,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void expand(final View view) {
         view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        final int targetHeight = view.getMeasuredHeight();
         //ValueAnimator valueAnimator = new ValueAnimator.ofInt(view.getMeasuredHeight(),pixelsToDp(expandedHeight));
-        ValueAnimator valueAnimator = ValueAnimator.ofInt(pixelsToDp(expandedHeight),view.getMeasuredHeight())
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(pixelsToDp(),view.getMeasuredHeight())
                 .setDuration(500);
 
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int animatedValue = (int) animation.getAnimatedValue();
-                ViewGroup.LayoutParams lp = view.getLayoutParams();
-                lp.height = animatedValue;
-                view.setLayoutParams(lp);
-                if (animatedValue == 0) {
-
-                }
-            }
+        valueAnimator.addUpdateListener(animation -> {
+            int animatedValue = (int) animation.getAnimatedValue();
+            ViewGroup.LayoutParams lp = view.getLayoutParams();
+            lp.height = animatedValue;
+            view.setLayoutParams(lp);
         });
 
         valueAnimator.start();
     }
 
     private class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
-        private ArrayList<Bitmap> images = new ArrayList<Bitmap>();
+        private final ArrayList<Bitmap> images;
 
-        public RecyclerAdapter(Context context, ArrayList<Bitmap> images) {
+        public RecyclerAdapter(ArrayList<Bitmap> images) {
             this.images = images;
         }
 
+        @NonNull
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             View view = getLayoutInflater().inflate(R.layout.recycler_item, parent, false);
-            ViewHolder viewHolder = new ViewHolder(view);
-            return viewHolder;
+            return new ViewHolder(view);
         }
 
         @Override
